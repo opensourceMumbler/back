295e2cdb23ecfed450b2b8ba835943d48b3900ba
이재성 <loveliest@cau.ac.kr>
2023-12-14 08:20:34 +0900

    get 완성

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.schema import HumanMessage
+from flask import Flask, request
+import datetime
+import pymysql
+import os
 
 load_dotenv()
 llm = OpenAI()
 
-text = "집에 가고 싶다. 퇴근하고 싶다."
-context = "라는 일기에 대해 감정분석을 해줘"
-messages = [HumanMessage(content=text+context)]
+db_connection = pymysql.connect(
+    user = 'admin',
+    passwd=os.environ['db_pw'],
+    host=os.environ['db_url'],
+    db='mumbler',
+    charset='utf8'
+)
 
-result = llm.invoke(text)
-print(result)
 
+@app.route('/search')
+def main_get():
+    db = db_connection()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    user = request.args.get("user")
+    now = request.args.get("now")
+    now_start = datetime.datetime.striptime(now, '%Y-%m-%d %H:%M:%S')
+    now_start.replace(hour=0, minute=0, second=0)
+
+    query = 'SELECT * FROM mumbles where user = {user} and timestamp > {now_start} and timestamp < {now}'
+    cursor.excute(query)
+    result = cursor.fetchall()
+
+    context = "라는 일기에 대해 감정분석을 해줘"
+    report = llm.invoke(result+context)
+    
+    return report
+
+
+@app.rout('/insert')
+def insert():
+    db = db_connection()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+
+    query = 'INSERT INTO mumbles () VALUES ()'
+    cursor.excute(query)
+    db.commit()
+    db.close()
+    
